7818983264f82860dc30ce6da439dbefd71a6966
Will Coyne <coyne.w@wustl.edu>
2026-03-29 16:45:48 -0500

    refactor: update Square component to accept value prop for dynamic rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 export default function Board() {
   return (
