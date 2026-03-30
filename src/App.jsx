a9f098d92818c7e92b97ad94418613b0b9d1f047
Will Coyne <coyne.w@wustl.edu>
2026-03-29 22:14:24 -0500

    created game and handle play function

--- src/App.jsx
+++ src/App.jsx
@@ -75,3 +75,19 @@ function calculateWinner(squares) {
   }
   return null;
 }
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        //...
+  )
+}
