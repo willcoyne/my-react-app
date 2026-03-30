8b588453660dae66f940c73c78eb951fe7b2ad8f
Will Coyne <coyne.w@wustl.edu>
2026-03-29 22:16:37 -0500

    feat: implement jumpTo function and moves list for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,23 @@ export default function Game() {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+ function jumpTo(nextMove) {
+    // TODO
+  }
 
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
